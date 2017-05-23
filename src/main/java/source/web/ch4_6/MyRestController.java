82e009d8889c5ae669c2220f4a306e6012fcc317
maoyi.xuan <maoyi.xuan@tongdun.cn>
2017-05-23 15:21:28 +0800

    code

---
+++ src/main/java/source/web/ch4_6/MyRestController.java
@@ -0,0 +1,21 @@
+package source.web.ch4_6;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import source.service.DemoService;
+
+@RestController
+public class MyRestController {
+	
+	@Autowired
+	DemoService demoService;
+	
+	@RequestMapping(value = "/testRest" ,produces="text/plain;charset=UTF-8")
+	public @ResponseBody String testRest(){
+		return demoService.saySomething();
+	}
+
+}
--- src/main/resources/views/upload.jsp
+++ src/main/resources/views/upload.jsp
@@ -4,11 +4,19 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title>Insert title here</title>
+<title>upload page</title>
+
 </head>
 <body>
-	<pre>
-		Welcome to Spring MVC world
-	</pre>
+
+
+<div class="upload">
+	<form action="upload" enctype="multipart/form-data" method="post">
+		<input type="file" name="file"/><br/>
+		<input type="submit" value="上传">
+	</form>
+</div>
+
+
 </body>
 </html>
